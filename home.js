6589cbe05074da714ba119e665fa72a437aeb0b2
alamin20cse <md.alamin165086@gmail.com>
2024-09-18 23:31:13 +0600

    complet

--- home.js
+++ home.js
@@ -1,7 +1,8 @@
 
 
+let sum=9400;
 document.getElementById('add-button').addEventListener('click',function(event){
-    let sum=9400;
+   
     event.preventDefault();
 
    const TakaButton=document.getElementById('taka-button');
@@ -20,6 +21,54 @@ document.getElementById('add-button').addEventListener('click',function(event){
    {
     alert('pin is wrong');
    }
+
+
+});
+
+
+
+// cash out
+
+let dis=9400;
+document.getElementById('cashout-button').addEventListener('click',function(event){
+   
+    event.preventDefault();
+
+   const TakaButton=document.getElementById('taka-button');
+   const AmountField=Number(document.getElementById('cashout-amount-field').value);
+
+   
+   const PinButton=document.getElementById('cashout-pin-button').value;
+
+   if(PinButton==='123')
+   {
+    dis=dis-AmountField;
+    TakaButton.innerText=dis;
+
+   }
+   else
+   {
+    alert('pin is wrong');
+   }
+
+
+});
+
+
+// =======================================
+// feature
+
+document.getElementById('show-cashout').addEventListener('click',function(){
+
+    document.getElementById('cashout-section').classList.remove('hidden');
+    document.getElementById('cashin-section').classList.add('hidden');
+});
+
+document.getElementById('show-cashin').addEventListener('click',function(){
+
+    
+    document.getElementById('cashout-section').classList.add('hidden');
+    document.getElementById('cashin-section').classList.remove('hidden');
 
 
 });
