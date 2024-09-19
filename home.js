c5034acb4ac261dd8f9568ea572f22fbbaa9c72f
alamin20cse <md.alamin165086@gmail.com>
2024-09-19 14:32:14 +0600

    all complet

--- home.js
+++ home.js
@@ -1,28 +1,20 @@
 
 
-let sum=9400;
-document.getElementById('add-button').addEventListener('click',function(event){
-   
+document.getElementById('add-button').addEventListener('click', function(event) {
     event.preventDefault();
 
-   const TakaButton=document.getElementById('taka-button');
-   const AmountField=Number(document.getElementById('amount-field').value);
+    const TakaButton = document.getElementById('taka-button'); // The HTML element
+    let TakaButtonnumber = Number(TakaButton.innerText); // The number from innerText
 
-   
-   const PinButton=document.getElementById('pin-button').value;
+    const AmountField = Number(document.getElementById('amount-field').value);
+    const PinButton = document.getElementById('pin-button').value;
 
-   if(PinButton==='123')
-   {
-    sum=sum+AmountField;
-    TakaButton.innerText=sum;
-
-   }
-   else
-   {
-    alert('pin is wrong');
-   }
-
-
+    if (PinButton === '123') {
+        TakaButtonnumber = TakaButtonnumber + AmountField; // Add the amount
+        TakaButton.innerText = TakaButtonnumber; // Update the innerText of the element
+    } else {
+        alert('pin is wrong');
+    }
 });
 
 
@@ -37,13 +29,15 @@ document.getElementById('cashout-button').addEventListener('click',function(even
    const TakaButton=document.getElementById('taka-button');
    const AmountField=Number(document.getElementById('cashout-amount-field').value);
 
+   let TakaButtonnumber = Number(TakaButton.innerText); // The number from innerText
+
    
    const PinButton=document.getElementById('cashout-pin-button').value;
 
    if(PinButton==='123')
    {
-    sum=sum-AmountField;
-    TakaButton.innerText=dis;
+    TakaButtonnumber = TakaButtonnumber - AmountField; // Add the amount
+    TakaButton.innerText = TakaButtonnumber; // Update the innerText of the element
 
    }
    else
